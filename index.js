55fbf899bef579a177b94a56fbff7d9e3d9660eb
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-08-31 21:17:08 -0400

    adding routes to get qrcode parameter

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const QR = require("./src/qr_code"); 
 const app = express();
 const port = process.env.PORT || 1801;
 
 app.use(express.json());
+//app.use(express.static('public'));
+
+app.get("/qr", (req, res)=>{
+    const code = req.query.code;
+    QR.generateQRCode(res, code);
+});
 
 app.get("/", (req, res)=>{
-    res.send("Hello World");
+    res.json({
+        server_response:{
+            error: "URL not found"
+        }
+    });
 });
 
 app.listen(port, ()=>{
