970d0a4411ce44e7e9c1a5cc56a04b5d59e5ce4c
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-09-07 23:58:27 -0400

    function getQrCode added

---
+++ public/js/getQrCode.js
@@ -0,0 +1,24 @@
+async function _getQrCode(event){
+    event.preventDefault();
+    event.stopPropagation();
+    let form = document.getElementById("qrform");
+    let formData = new FormData(form);
+    let data = Object.fromEntries(formData.entries());
+ 
+    console.log(data);
+    if(data.code.trim() === ""){
+        const urlParams = new URLSearchParams(data);
+        console.log(data);
+        const url = `https://qrcodes-api.herokuapp.com/qr?${urlParams}`;
+        //console.log(url);
+        
+        const request = await fetch(url);
+        let response = null;
+        if(request.ok){
+            response = await request.json();
+        }
+        console.table(response);
+    }else{
+        console.log('Empty parameter');
+    }
+}
