d75412ab9de5c4345a4b4e81302bdd6bea5a24d4
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-09-10 00:22:16 -0400

    setting target in function _getQrCode

--- public/js/getQrCode.js
+++ public/js/getQrCode.js
@@ -1,23 +1,39 @@
-async function _getQrCode(event){
+async function _getQrCode(event, qr_img){
     event.preventDefault();
     event.stopPropagation();
+    showLoader('result_box','qr_code');
     let form = document.getElementById("qrform");
     let formData = new FormData(form);
     let data = Object.fromEntries(formData.entries());
-    //console.log(data);
-
+    
     if(data.code.trim() !== ""){
         const urlParams = new URLSearchParams(data);
-        console.log(data);
         const url = `https://qrcodes-api.herokuapp.com/qr?${urlParams}`;
-        //console.log(url);
-        const request = await fetch(url);
-        let response = null;
-        if(request.ok){
-            response = await request.json();
+        try{
+            const request = await fetch(url);
+            let response = null;
+            let _qr_img = document.getElementById(`${qr_img}`);
+            
+            if(request.ok){
+                showLoader('result_box','qr_code', false);
+                response = await request.json();
+                response = response.server_response;
+                if(!_qr_img) return;
+                _qr_img.setAttribute('src',`${response.qr_code}`);
+                console.table({response: true});
+                return;
+            }
+        }catch(error){
+            setTimeout(()=>{
+                showLoader('result_box','qr_code', false);
+            },500); 
+            console.table({Error: `${error}`});
+            return;
         }
-        console.table(response);
     }else{
+        setTimeout(()=>{
+            showLoader('result_box','qr_code', false);
+        },500); 
         console.log('Empty parameter');
     }
 }
